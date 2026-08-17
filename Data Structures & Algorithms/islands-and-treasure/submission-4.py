44fb7e60bd1fcc323f41a9c2674217898a0777d5
gkriskwon <164305961+gkriskwon@users.noreply.github.com>
2026-08-16 22:00:07 -0700

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        # start from 0 instead of INF
+        INF = 2147483647
+        ROWS, COLS = len(grid), len(grid[0])        
+
+        q = collections.deque()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append((r, c))
+                    # explore land from the chest till reach water
+
+        while q:
+            r, c = q.popleft()
+        
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = r + dr, c + dc
+                if (
+                    0 <= nr < ROWS 
+                    and 0 <= nc < COLS
+                    and grid[nr][nc] == INF
+                ):
+                    grid[nr][nc] = grid[r][c] + 1
+                    q.append((nr, nc))
+                     
+
+
