bf52212d48dfb82d565b44c1c9db2e09075ffff8
gkriskwon <164305961+gkriskwon@users.noreply.github.com>
2026-08-17 15:24:43 -0700

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, numbers: List[int]) -> List[List[int]]:
+        # Brute force O(N^3)
+        # No need to check length due to constraints
+        new_numbers = sorted(numbers)
+        n = len(new_numbers)
+        result = set()
+        for i in range(n - 2):
+            
+            start = i + 1
+            end = n - 1
+            target = -new_numbers[i]
+
+            while start < end:
+                current_sum = new_numbers[start] + new_numbers[end]
+
+                if current_sum < target:
+                    start += 1
+                elif current_sum > target:
+                    end -= 1
+                else:
+                    result.add((new_numbers[i], new_numbers[start], new_numbers[end]))
+                    start += 1
+                    end -= 1
+
+        return list(result)
+
+        
